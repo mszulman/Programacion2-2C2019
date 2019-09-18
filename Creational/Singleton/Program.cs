35c8282c6267f78aa2e758fb2ca2b1b34f9bbc37
Matias Szulman <mszulman@ort.edu.ar>
2019-09-18 20:55:40 -0300

    Implemented Singleton Pattern

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Singleton.GetInstance();
+
+            Console.WriteLine(s.GetValorActual());
+            s.Incrementar();
+            s.Incrementar();
+            s.Incrementar();
+
+            Console.WriteLine(s.GetValorActual());
+
+            var x = Singleton.GetInstance();
+            x.Incrementar();
+            x.Incrementar();
+            x.Incrementar();
+            Console.WriteLine(x.GetValorActual());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Creational/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private int _contador;
+
+        private static Singleton _instance;
+        public static Singleton GetInstance()
+        {
+            if (_instance is null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+        private Singleton()
+        {
+            _contador = 0;
+        }
+
+        public void Incrementar()
+        {
+            _contador++;
+        }
+
+        public string GetValorActual()
+        {
+            return "El valor actual es " + _contador.ToString();
+        }
+    }
+}
